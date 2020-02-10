431df0c7b92a06d48a5a0bbfb6b3feb69b2014e5
eduosi <webmaster@buession.com>
2020-02-10 23:25:11 +0800

    优化 banner

--- buession-springboot-boot/src/main/java/com/buession/springboot/boot/banner/AbstractBanner.java
+++ buession-springboot-boot/src/main/java/com/buession/springboot/boot/banner/AbstractBanner.java
@@ -21,13 +21,14 @@
  * +------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										|
  * | Author: Yong.Teng <webmaster@buession.com> 													|
- * | Copyright @ 2013-2019 Buession.com Inc.														|
+ * | Copyright @ 2013-2020 Buession.com Inc.														|
  * +------------------------------------------------------------------------------------------------+
  */
 package com.buession.springboot.boot.banner;
 
 import com.buession.core.Framework;
 import com.buession.core.utils.StringUtils;
+import com.buession.core.utils.VersionUtils;
 import com.github.lalyos.jfiglet.FigletFont;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.boot.Banner;
@@ -46,121 +47,98 @@ import java.util.Properties;
  */
 public abstract class AbstractBanner implements Banner {
 
-    private final static String TITLE = Framework.NAME.toUpperCase();
+	private final static int SEPARATOR_REPEAT_COUNT = 60;
 
-    private final static int SEPARATOR_REPEAT_COUNT = 60;
+	private final static String LINE_SEPARATOR = String.join(StringUtils.EMPTY, Collections.nCopies
+			(SEPARATOR_REPEAT_COUNT, "-"));
 
-    private final static String LINE_SEPARATOR = String.join("", Collections.nCopies(SEPARATOR_REPEAT_COUNT, "-"));
+	@Override
+	public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out){
+		String additional = collectEnvironmentInfo(environment, sourceClass);
 
-    private final static Formatter formatter = new Formatter();
+		try{
+			out.println("\u001b[36m");
+			out.println(FigletFont.convertOneLine(getTitle()));
 
-    private final static Properties properties = System.getProperties();
+			if(StringUtils.isNotBlank(additional) == true){
+				out.println(additional);
+			}
 
-    @Override
-    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out){
-        printAsciiArt(out, getTitle(), collectEnvironmentInfo(environment, sourceClass));
-    }
+			out.println("\u001b[0m");
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
 
-    protected String getTitle(){
-        return "(" + TITLE + ")";
-    }
+	protected String getTitle(){
+		return "(" + Framework.NAME.toUpperCase() + ")";
+	}
 
-    protected String getVersion(){
-        return getVersion(AbstractBanner.class);
-    }
+	protected String getVersion(){
+		return getVersion(AbstractBanner.class);
+	}
 
-    protected <T> String getVersion(Class<T> clazz){
-        if(clazz == null){
-            return null;
-        }
+	protected <T> String getVersion(Class<T> clazz){
+		return clazz == null ? null : VersionUtils.determineClassVersion(clazz);
+	}
 
-        Package pkg = clazz.getPackage();
-        return pkg != null ? pkg.getImplementationVersion() : null;
-    }
+	private String collectEnvironmentInfo(Environment environment, Class<?> sourceClass){
+		Properties properties = System.getProperties();
+		Throwable throwable = null;
 
-    private String collectEnvironmentInfo(Environment environment, Class<?> sourceClass){
-        Throwable throwable = null;
-        String result;
+		Formatter formatter = new Formatter();
 
-        formatter.format("Project Version: %s%n", new Object[]{getVersion()});
-        formatter.format("Buession Framework Version: %s%n", new Object[]{getVersion(Framework.class)});
+		formatter.format("Project Version: %s%n", new Object[]{getVersion()});
+		formatter.format("Buession Framework Version: %s%n", new Object[]{getVersion(Framework.class)});
 
-        if(properties.containsKey("BANNER_SKIP")){
-            try{
-                result = formatter.toString();
-            }catch(Throwable t){
-                throwable = t;
-                throw t;
-            }finally{
-                closeFormatter(throwable);
-            }
-        }else{
-            try{
-                formatter.format("Spring Boot Framework Version: %s%n", new Object[]{SpringBootVersion.getVersion()});
-                formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
-                formatter.format("System Date/Time: %s%n", new Object[]{LocalDateTime.now()});
-                formatter.format("System Temp Directory: %s%n", new Object[]{properties.get("java.io.tmpdir")});
-                formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
-                formatter.format("Java Home: %s%n", new Object[]{properties.get("java.home")});
-                formatter.format("Java Vendor: %s%n", new Object[]{properties.get("java.vendor")});
-                formatter.format("Java Version: %s%n", new Object[]{properties.get("java.version")});
-                formatter.format("JCE Installed: %s%n", new Object[]{BooleanUtils.toStringYesNo(isJceInstalled())});
-                formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
-                formatter.format("OS Architecture: %s%n", new Object[]{properties.get("os.arch")});
-                formatter.format("OS Name: %s%n", new Object[]{properties.get("os.name")});
-                formatter.format("OS Version: %s%n", new Object[]{properties.get("os.version")});
-                formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
+		try{
+			if(properties.containsKey("BANNER_SKIP") == false){
+				formatter.format("Spring Boot Framework Version: %s%n", new Object[]{SpringBootVersion.getVersion()});
+				formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
+				formatter.format("System Date/Time: %s%n", new Object[]{LocalDateTime.now()});
+				formatter.format("System Temp Directory: %s%n", new Object[]{properties.get("java.io.tmpdir")});
+				formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
+				formatter.format("Java Home: %s%n", new Object[]{properties.get("java.home")});
+				formatter.format("Java Vendor: %s%n", new Object[]{properties.get("java.vendor")});
+				formatter.format("Java Version: %s%n", new Object[]{properties.get("java.version")});
+				formatter.format("JCE Installed: %s%n", new Object[]{BooleanUtils.toStringYesNo(isJceInstalled())});
+				formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
+				formatter.format("OS Architecture: %s%n", new Object[]{properties.get("os.arch")});
+				formatter.format("OS Name: %s%n", new Object[]{properties.get("os.name")});
+				formatter.format("OS Version: %s%n", new Object[]{properties.get("os.version")});
+				formatter.format("%s%n", new Object[]{LINE_SEPARATOR});
 
-                injectEnvironmentInfoIntoBanner(formatter, environment, sourceClass);
-                result = formatter.toString();
-            }catch(Throwable t){
-                throwable = t;
-                throw t;
-            }finally{
-                closeFormatter(throwable);
-            }
-        }
+				injectEnvironmentInfoIntoBanner(formatter, environment, sourceClass);
+			}
 
-        return result;
-    }
+			return formatter.toString();
+		}catch(Throwable t){
+			throwable = t;
+			throw t;
+		}finally{
+			closeFormatter(formatter, throwable);
+		}
+	}
 
-    protected void injectEnvironmentInfoIntoBanner(Formatter formatter, Environment environment, Class<?> sourceClass){
-    }
+	protected void injectEnvironmentInfoIntoBanner(Formatter formatter, Environment environment, Class<?> sourceClass){
+	}
 
-    private final static boolean isJceInstalled(){
-        try{
-            return Cipher.getMaxAllowedKeyLength("AES") == 2147483647;
-        }catch(Exception e){
-            return false;
-        }
-    }
+	private final static boolean isJceInstalled(){
+		try{
+			return Cipher.getMaxAllowedKeyLength("AES") == 2147483647;
+		}catch(Exception e){
+			return false;
+		}
+	}
 
-    private final static void printAsciiArt(PrintStream out, String asciiArt, String additional){
-        try{
-            out.println("\u001b[36m");
-            if(StringUtils.isNotBlank(additional) == true){
-                out.println(FigletFont.convertOneLine(asciiArt));
-                out.println(additional);
-            }else{
-                out.print(FigletFont.convertOneLine(asciiArt));
-            }
-
-            out.println("\u001b[0m");
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    private final static void closeFormatter(Throwable throwable){
-        if(throwable != null){
-            try{
-                formatter.close();
-            }catch(Throwable t){
-                throwable.addSuppressed(t);
-            }
-        }else{
-            formatter.close();
-        }
-    }
+	private final static void closeFormatter(Formatter formatter, Throwable throwable){
+		try{
+			formatter.close();
+		}catch(Throwable t){
+			if(throwable != null){
+				throwable.addSuppressed(t);
+			}
+		}
+	}
 
 }
