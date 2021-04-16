cf12c945042be46a9d837da69b151d586145ac3b
Yong.Teng <webmaster@buession.com>
2021-04-16 23:29:58 +0800

    优化 AbstractCliApplication 日志打印

--- buession-springboot-cli/src/main/java/com/buession/springboot/cli/application/AbstractCliApplication.java
+++ buession-springboot-cli/src/main/java/com/buession/springboot/cli/application/AbstractCliApplication.java
@@ -19,7 +19,7 @@
  * +-------------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
- * | Copyright @ 2013-2020 Buession.com Inc.														       |
+ * | Copyright @ 2013-2021 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
  */
 package com.buession.springboot.cli.application;
@@ -59,9 +59,11 @@ public abstract class AbstractCliApplication extends AbstractApplication impleme
 			springApplicationBuilder.contextClass(getConfigurableApplicationContext());
 		}
 
-		springApplicationBuilder.web(WebApplicationType.NONE).properties(createRuntimeProperties()).logStartupInfo
-				(true).run(args);
-		logger.info("Application startup at {}", new Date());
+		springApplicationBuilder.web(WebApplicationType.NONE).properties(createRuntimeProperties()).logStartupInfo(true).run(args);
+
+		if(logger.isInfoEnabled()){
+			logger.info("Application startup at {}", new Date());
+		}
 	}
 
 	@Override
